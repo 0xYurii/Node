2ceed9ea6f8e15f7650c902a48c50c2e02ebce71
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-04 20:21:33 +0100

    set the 404 message

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const server = http.createServer(async(req, res) => {
     res.end(JSON.stringify(destinations))
     
   } else {
-    res.end('Not found')
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode=404
+    res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
   }
 })
 
