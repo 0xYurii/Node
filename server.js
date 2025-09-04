268b730f28c65b0cc70112b9f0c61fecf333cc77
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-04 15:25:43 +0100

    creat first server

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import http from 'node:http'
 
-const PORT = 8000
+const PORT=8000
 
-/*
-Challenge:
-1. Recreate our server so we send a string over http when a GET request comes in. 
-2. Test it by making a GET request to http://localhost:8000 in the network widget.
-See hint.md for prompts.
-*/
+const server=http.createServer((req,res)=>{
+    res.end('Hello from the server!')
+})
+
+server.listen(PORT, () => console.log(`server running on port: ${PORT}`))
