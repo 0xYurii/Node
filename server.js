396bf2c434dc1ca783af433c87c5e8609bd96273
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-04 16:17:12 +0100

    add conditions

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import http from 'node:http'
 
-const PORT=8000
+const PORT = 8000
 
-const server=http.createServer((req,res)=>{
-    res.end('Hello from the server!')
+
+const server = http.createServer( (req, res)=> {
+
+  console.log(req.url)
+
+/*
+Challenge:
+Check the ‘url’ property on the req object. 
+Only serve our string if it’s ‘/ api’.
+*/
+
+  {req.url==="/api" ?res.end('This is from the server'):res.end('Not found')}
+
 })
 
-server.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
