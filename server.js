5d5da99af7da62bfa3529d5e4e975fec3a34327e
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-04 16:49:52 +0100

    fetch data from api

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 import http from 'node:http'
+import { getDataFromDB } from './ds.js'
 
 const PORT = 8000
 
 
-const server = http.createServer( (req, res)=> {
+const server = http.createServer(async (req, res)=> {
 
   console.log(req.url)
 
 /*
 Challenge:
-Check the ‘url’ property on the req object. 
-Only serve our string if it’s ‘/ api’.
+  1. Store our data in a const ‘destinations’.
+  2. When a GET request is received to the ‘/api' endpoint, send our JSON stringified data.
+    Think: What changes will you need to make to get this to work?
 */
+  const destination= await getDataFromDB()
 
-  {req.url==="/api" ?res.end('This is from the server'):res.end('Not found')}
+  {req.url==="/api" ?
+    res.end(JSON.stringify(destination)):
+    res.end('Not found')}
+
 
 })
 
